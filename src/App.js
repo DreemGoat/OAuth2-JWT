0087cf80ca48d8af68e300d5d095dbbc54b53b3b
Jocelin21 <jocelin.wilson@binus.ac.id>
2023-05-26 16:18:27 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
-import SignUp from "./components/SignUp"
-
+import SignUp from "./components/SignUp";
 
 //uncomment down below to see other pages
 //it starts getting weird the images, pls help
@@ -14,38 +12,36 @@ import Login from "./components/Login";
 import Welcome from "./components/Welcome";
 import Home from "./components/Home";
 import Chat from "./components/Chat";
-import Explore from "./components/Explore"
+import Explore from "./components/Explore";
 import Dashboard from "./components/Dashboard";
-import OAuthPopup from "./components/OAuth2Popup"
+import OAuthPopup from "./components/OAuth2Popup";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 function App() {
-    return (
-      
+  return (
     <div className="app">
       <Router>
         <Routes>
-          <Route element = {<OAuthPopup />} path = "/callback" />
+          <Route element={<OAuthPopup />} path="/callback" />
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<SignUp />} /> 
+          <Route exact path="/signup" element={<SignUp />} />
           {/* uncomment down below to see the other pages */}
 
-          <Route exact path="/humandetails" element={<HumanProfile />} /> 
-          <Route exact path="/petdetails" element={<PetDetails />} /> 
-          <Route exact path="/petbio" element={<PetBio />} /> 
+          <Route exact path="/humandetails" element={<HumanProfile />} />
+          <Route exact path="/petdetails" element={<PetDetails />} />
+          <Route exact path="/petbio" element={<PetBio />} />
           <Route exact path="/start" element={<Start />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/match" element={<Match />} />
           <Route exact path="/chat" element={<Chat />} />
           <Route exact path="/explore" element={<Explore />} />
-          <Route exact path="/welcome" element={<Welcome/>} />
+          <Route exact path="/welcome" element={<Welcome />} />
         </Routes>
       </Router>
     </div>
-    )
-  }
+  );
+}
 
 export default App;
