4d6f9cfd63e80f3d4f61b06039fd05152b20c70a
Jocelin21 <jocelin.wilson@binus.ac.id>
2023-05-26 15:37:47 +0700

    commit

--- src/components/OAuth2Popup.jsx
+++ src/components/OAuth2Popup.jsx
@@ -1,115 +1,53 @@
-<<<<<<< HEAD
 import { useEffect } from 'react';
+import { queryToObject } from './tools.ts';
+
 
 const OAUTH_STATE_KEY = 'react-use-oauth2-state-key';
 const OAUTH_RESPONSE = 'react-use-oauth2-response';
 
 const checkState = (receivedState) => {
-	const state = sessionStorage.getItem(OAUTH_STATE_KEY);
-	return state === receivedState;
-};
-
-const queryToObject = (query) => {
-	const parameters = new URLSearchParams(query);
-	return Object.fromEntries(parameters.entries());
+  const state = sessionStorage.getItem(OAUTH_STATE_KEY);
+  return state === receivedState;
 };
 
 const OAuthPopup = (props) => {
-	const {
-		Component = (
-			<div style={{ margin: '12px' }} data-testid="popup-loading">
-				Loading...
-			</div>
-		),
-	} = props;
+  const {
+    Component = (
+      <div style={{ margin: '12px' }} data-testid="popup-loading">
+        Loading...
+      </div>
+    ),
+  } = props;
 
-	// On mount
-	useEffect(() => {
-		const payload = queryToObject(window.location.search.split('?')[1]);
-		const state = payload && payload.state;
-		const error = payload && payload.error;
+  // On mount
+  useEffect(() => {
+    const payload = queryToObject(window.location.search.split('?')[1]);
+    const state = payload && payload.state;
+    const error = payload && payload.error;
 
-		if (!window.opener) {
-			throw new Error('No window opener');
-		}
+    if (!window.opener) {
+      throw new Error('No window opener');
+    }
 
-		if (error) {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				error: decodeURI(error) || 'OAuth error: An error has occured.',
-			});
-		} else if (state && checkState(state)) {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				payload,
-			});
-		} else {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				error: 'OAuth error: State mismatch.',
-			});
-		}
-	}, []);
+    if (error) {
+      window.opener.postMessage({
+        type: OAUTH_RESPONSE,
+        error: decodeURI(error) || 'OAuth error: An error has occurred.',
+      });
+    } else if (state && checkState(state)) {
+      window.opener.postMessage({
+        type: OAUTH_RESPONSE,
+        payload,
+      });
+    } else {
+      window.opener.postMessage({
+        type: OAUTH_RESPONSE,
+        error: 'OAuth error: State mismatch.',
+      });
+    }
+  }, []);
 
-	return Component;
+  return Component;
 };
 
-=======
-import { useEffect } from 'react';
-import { queryToObject } from './tools';
-
-const OAUTH_STATE_KEY = 'react-use-oauth2-state-key';
-const OAUTH_RESPONSE = 'react-use-oauth2-response';
-
-const checkState = (receivedState) => {
-	const state = sessionStorage.getItem(OAUTH_STATE_KEY);
-	return state === receivedState;
-};
-
-const queryToObject = (query) => {
-	const parameters = new URLSearchParams(query);
-	return Object.fromEntries(parameters.entries());
-};
-
-const OAuthPopup = (props) => {
-	const {
-		Component = (
-			<div style={{ margin: '12px' }} data-testid="popup-loading">
-				Loading...
-			</div>
-		),
-	} = props;
-
-	// On mount
-	useEffect(() => {
-		const payload = queryToObject(window.location.search.split('?')[1]);
-		const state = payload && payload.state;
-		const error = payload && payload.error;
-
-		if (!window.opener) {
-			throw new Error('No window opener');
-		}
-
-		if (error) {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				error: decodeURI(error) || 'OAuth error: An error has occured.',
-			});
-		} else if (state && checkState(state)) {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				payload,
-			});
-		} else {
-			window.opener.postMessage({
-				type: OAUTH_RESPONSE,
-				error: 'OAuth error: State mismatch.',
-			});
-		}
-	}, []);
-
-	return Component;
-};
-
->>>>>>> 865bd9ac980701c00cdf456b73588387f4e3bbbd
 export default OAuthPopup;
