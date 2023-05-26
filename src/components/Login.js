6743c6c8fdc157ccd4f99ed6c77d3f2ddea5796f
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2023-05-26 21:41:53 +0700

    Added Auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import AuthContext from "./AuthProvider"
+import { Link, useNavigate } from "react-router-dom";
+
+import axios from './api/axios'
 import "../styles/Login.css";
 import ErrorMessage from "./ErrorMessage";
-import { OAuthPopup, useOAuth2 } from "@tasoskakour/react-use-oauth2";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [token, setToken] = useState(""); // Define your TokenContext here
+  const [, setToken] = useContext("");
 
   const submitLogin = async () => {
     const requestOptions = {
@@ -22,87 +24,41 @@ const Login = () => {
     const response = await fetch("/api/token", requestOptions);
     const data = await response.json();
 
-    if (!response.ok) {
-      setErrorMessage(data.detail);
-    } else {
-      setToken(data.access_token);
-    }
+    // if (!response.ok) {
+    //   setErrorMessage(data.detail);
+    // } else {
+    //   setToken(data.access_token);
+    // } 
+    //token
   };
 
-  const { data, loading, error, getAuth, logout } = useOAuth2({
-    authorizeUrl: "https://example.com/auth", //change
-    clientId: "YOUR_CLIENT_ID",
-    redirectUri: `${document.location.origin}/callback`,
-    scope: "YOUR_SCOPES",
-    responseType: "code",
-    exchangeCodeForTokenServerURL: "https://your-backend/token",
-    exchangeCodeForTokenMethod: "POST",
-    onSuccess: (payload) => console.log("Success", payload),
-    onError: (error_) => console.log("Error", error_)
-  });
-
-  const isLoggedIn = Boolean(data?.access_token); // or whatever...
-
-  if (error) {
-    return <div>Error</div>;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isLoggedIn) {
-    return (
-      <div>
-        <pre>{JSON.stringify(data)}</pre>
-        <button onClick={logout}>Logout</button>
-      </div>
-    )
-  }
-
   return (
-    <>
     <div>
+      {/* <style>
+        body {
+            background: url("../authenticationbg.png";)
+        }
+      </style> */}
       <meta charSet="utf-8" />
       <title>Sign In</title>
       <Link rel="stylesheet" href="styles/authentication.css" />
       <div className="auth-container">
         <div className="auth2-container">
+
           <div className="mail">Email</div>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email address"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
+          <input type="email" id="email" name="email" placeholder="Enter your email address" onChange={(e) => setEmail(e.target.value)}/>
+          
           <div className="pass">Password</div>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" id="password" name="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}/>
+          
+          <a href="dashboard"><button className="buttonsignup">Log in</button></a>
+          <a href="dashboard"><button className="buttonoauth">Log in with OAuth2</button></a>
+          <p className="signmessage">Don't have an account yet? <a href="signup" className="bold-italic">Sign up</a> now!</p>
 
-          <button className="buttonsignup" onClick={submitLogin}>
-            Log in
-          </button>
-          <button className="buttonoauth" type="button" onClick={() => getAuth()}>
-            Log in with OAuth2
-          </button>
-          <p className="signmessage">
-            Don't have an account yet?{" "}
-            <Link to="/signup" className="bold-italic">
-              Sign up
-            </Link>{" "}
-            now!
-          </p>
+
         </div>
       </div>
     </div>
-    </>
   );
 };
 
